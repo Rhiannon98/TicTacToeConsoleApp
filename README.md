8f141146fa756899819cc5e177d3db1f195e2cd4
mortensenrhi <mortensenrhi@gmail.com>
2018-06-25 08:42:14 -0700

    beginning, only thing i could do on train

---
+++ README.md
@@ -0,0 +1,24 @@
+## Tic Tac Toe
+
+**Author** : Rhiannon Mortensen
+**Version** : 1.0.0
+
+## Overview :
+  This simple console application is to showcase  
+  classes and objects, how they are and can be connected,  
+  as well as how they can be implemented to make a game
+
+## Requirements to Play :
+1. Visual Studio 2017 
+2. The .NET desktop development workload enabled
+3. No External NuGet packages are required for this application. 
+
+## Simplified Example of Play : 
+
+## Architecture :
+This application is created using ASP.NET Core 2.1 Console applicaitons. <br />
+*Language*: C# <br />
+*Type of Applicaiton*: Console Application <br />sGame
+
+## External Sources :
+- 
---
+++ TicTacToe/TicTacToe/Classes/GameBoard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Classes
+{
+    class GameBoard
+    {
+        public string[][] Board { get; set; }
+        public string[][] Picked { get; set; }
+        public string[][] defaultBoard = new string[][]
+            {
+                new string[] {"1","2","3"},
+                new string[] {"4","5","6"},
+                new string[] {"7","8","9"}
+            };
+        public string[][] defaultPick = new string[][]
+        {
+            new string[3],
+            new string[3],
+            new string[3]
+        };
+        public GameBoard()
+        {
+            Board = defaultBoard;
+            Picked = defaultPick;
+        }
+        public bool DisplayGameBoard(string[][] board)
+        {
+            if (board.Length !=3){return false;}
+            try
+            {
+                Console.WriteLine("Current game:");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Board could not be made: {e.Message}");
+                return false;
+            }
+
+        }
+    }
+}
